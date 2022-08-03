8990104d69ed511c57db4bc6a1d00d6b585f4733
Peter Morris <mrpmorris@gmail.com>
2022-08-03 10:47:39 +0100

    Added ForEachWithIndex

---
+++ Source/Lib/Morris.Blazor.ControlFlow/ValueWithIndex.cs
@@ -0,0 +1,13 @@
+﻿namespace Morris.Blazor.ControlFlow;
+
+public readonly struct ValueWithIndex<T>
+{
+	public T Value { get; } = default!;
+	public int Index { get; } = 0;
+
+	public ValueWithIndex(T value, int index)
+	{
+		Index = index;
+		Value = value;
+	}
+}
